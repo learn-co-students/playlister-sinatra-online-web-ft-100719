93c1d44d52d9c3a329485948f6c7ce24cfc7fe97
your_name <raul.a.figueroa@hotmail.com>
2019-11-20 18:02:36 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,20 @@
+class SongsController < ApplicationController
+
+    get '/songs' do
+
+    end
+
+    get '/songs/:slug' do 
+    
+    end
+
+    get '/songs/:slug/edit' do 
+
+    end
+
+    get '/songs/new' do
+
+    end
+
+
+end
---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base 
+    belongs_to :artist
+    has_many :song_genres
+    has_many :genres, through: :song_genres
+    
+    def slug
+        self.name.gsub(" ", "-").downcase
+    end
+
+    def self.find_by_slug(slug)
+        name = slug.gsub("-", " ").titleize
+        self.all.find{|artist| artist.slug == slug}
+        #binding.pry
+    end
+
+ end
